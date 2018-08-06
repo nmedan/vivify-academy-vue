b8bbfeeaec89918e7c41127b361e2e956174f4e4
nmedan <nmedan92@gmail.com>
2018-08-06 20:36:33 +0200

    Created form for adding new contact

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h1>Contact list</h1>
+            <label>Name: </label>
+            <input v-model="newContact.name" type="text" placeholder="Name"><br>
+            <label>Email: </label>
+            <input v-model="newContact.email" type="text" placeholder="Email"><br>
+            <button type="submit" @click="AddContact">Add contact</button>
+        </form>
         <table>
             <thead>
                 <th>Name</th>
@@ -24,8 +30,21 @@ export default {
           contacts:[
               {name: "John", email: "example@gmail.com"},
               {name: "Jack", email: "example2@gmail.com"}
-          ]
-      }
-  }
+          ],
+          newContact: {
+              name: '',
+              email: ''
+          }
+      };
+    },
+          methods: {
+              AddContact() {
+                  console.log(this.newContact);
+                  this.contacts.push(this.newContact);
+                  this.newContact = {};
+              }
+          }
+      
+ 
 }
 </script>
