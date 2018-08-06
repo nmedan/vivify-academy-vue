c5a1b9584800537021c10b5882b5f80d817845ad
nmedan <nmedan92@gmail.com>
2018-08-06 20:09:03 +0200

    Added contacts

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,5 +1,31 @@
 <template>
     <div>
         <h1>Contact list</h1>
+        <table>
+            <thead>
+                <th>Name</th>
+                <th>Email </th>
+            </thead>
+            <tbody>
+                 <tr v-for="(contact, key) in contacts" :key="key">
+                      <td>{{contact.name}}</td>
+                      <td>{{contact.email}}</td>
+                 </tr>
+            </tbody>
+        </table>
     </div>
 </template>
+
+<script>
+export default {
+  name: 'ContactList',
+  data() {
+      return {
+          contacts:[
+              {name: "John", email: "example@gmail.com"},
+              {name: "Jack", email: "example2@gmail.com"}
+          ]
+      }
+  }
+}
+</script>
